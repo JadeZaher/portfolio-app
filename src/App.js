b51e4e018cf87fb81fc695fbf3b4a7d6e0628746
Ahmed Zaher <contact@ahmedzaher.net>
2022-04-29 06:22:57 -0700

    all

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='nav-wrap'>
+        <div className='nav'>
+          <a href=""> About </a>
+          <a href=""> Projects </a>
+          <a href=""> Blog </a>
+          <a href=""> Contact </a>
+        </div>
+      </div>
+      <div className='button-nav-right'>
+        <button> {'>'} </button>
+      </div>
+      <div className=''>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
