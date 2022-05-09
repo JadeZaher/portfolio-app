75ef8655caaa192f71e42ec6e687e0284b489799
ahmed zaher <contact@ahmedzaher.net>
2022-05-09 16:50:55 -0700

    Finally works. Server needed to flush cash + htaccess file was needed

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,42 +1,49 @@
+import Breakthrough from './Images/Breakthrough.png'
+import ComeAlongSide from './Images/ComeAlongSide.png'
+import CareFromAnywhere from './Images/CareFromAnywhere.png'
+import MyCoach from './Images/MyCoach.png'
+import PCAssessment from './Images/PCAssessment.png'
+import TresRiosTrust from './Images/TresRiosTrust.png'
+
 
 export const ProjectData = [
     {
-            image: `${process.env.PUBLIC_URL + '/images/ComeAlongSide.png'}`,
+            image: ComeAlongSide,
             heading: "ComeAlongSide",
             id: 1,
             content: "A Voice Chat enabled VR Application for Health and Fitness.",
             "moreContent": "ComeAlongSide is a VR application built using C# in Unity, with a cross-platform Voice chat integration using photon voice. On entering the experience the user would be able to use a room ID to create or join a room where they could voice chat with a loved one or caretaker, as they would both be immersed in a guided meditation experience."
         },
         {
-            image: `${process.env.PUBLIC_URL + '/images/MyCoach.png'}`,
+            image: MyCoach,
             heading: "MyCoach",
             id: 2,
             content: "An Alexa Voice skill for Health and Fitness",
             "moreContent":"MyCoach is an Alexa Voice skill that allows the user to trigger video plays by voice command on any Alexa-enabled video device. The app consists of an endpoint that hosts the functionality for each command built with the Node.js javascript runtime environment. Any of the commands would trigger an API call to Alexa servers, which would return a response indicating UI information, video streaming data, and Alexa’s voice response. The skill also provides the option for a user to get routines e-mailed to them, that would set them up with reminders and affirmations for their day."
         },
         {
-            image: `${process.env.PUBLIC_URL + '/images/PCAssessment.png'}`,
+            image: PCAssessment,
             heading: "The PC Assessment",
             id: 4,
             content: "A dynamic intake form for a regenerative therapy clinic",
             "moreContent":"An intake form to aid in categorizing new and prospective clients by their needs and goals, and label them accordingly in a CRM system. This was done on WIX using their Velo API, and Node.js. Implementing this update drastically reduced the time it would take to onboard new clients, and helped the Pacific Clinic further tailor its services to each individual.            "
         },        
         {
-            image: `${process.env.PUBLIC_URL + '/images/CareFromAnywhere.png'}`,
+            image: CareFromAnywhere,
             heading: "CareFromAnywhere",
             id: 5,
             content: "A UI/UX prototype for an in-home Care finder application.",
             "moreContent":"A mobile App prototype built using Adobe XD for an in-home care service. In development at The Pacific Clinic, for their non-profit CareFromAnywhere, the goal of this app was to take the service model that Uber has for transportation and apply it to healthcare. I worked with the Executive director of the clinic to build this design."
         },
         {
-            image: `${process.env.PUBLIC_URL + '/images/Breakthrough.png'}`,
+            image: Breakthrough,
             heading: "Breakthrough",
             id: 6,
             content: "A UI/UX prototype for timesheets, medication management, and behavior tracking.",
             "moreContent":"While working for Breakthrough Inc I created a prototype for a tool that aids in medication management, behavior tracking, and time tracking efforts at Breakthrough. Breakthrough Inc is a care facility for teens and young adults with intellectual disabilities and challenging behaviors. I saw serious shortcomings in the paper tracking system and sought to do what I can help with this pain point. My current goal is to continue working with Breakthrough, hoping to take this app into development."
         },
         {
-            image: `${process.env.PUBLIC_URL + '/images/TresRiosTrust.png'}`,
+            image: TresRiosTrust,
             heading: "Tres Rios Community Landtrust",
             id: 7,
             content: "Co-Founder and Technical Director for the Tres Rios Community Landtrust based in Tri-Cities WA",
